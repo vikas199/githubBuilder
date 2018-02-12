56d8f803296fb42edf121a04e1b512b14794d683
charan262 <charan.madhi@gmail.com>
2018-02-12 12:15:11 -0600

    added async calls

--- app/components/results.js
+++ app/components/results.js
@@ -1,11 +1,69 @@
-var React = require('react')
+var React = require('react');
+var queryString = require('query-string');
+var api = require('../utils/Api');
+var Link = require('react-router-dom').Link;
+
 
 class Results extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            winner: null,
+            loser:null,
+            error:null,
+            loading:true
+        }
+    }
+
+    componentDidMount(){
+        var players = queryString.parse(this.props.location.search);
+        api.battle([
+            players.playeroneName,
+            players.playertwoname
+        ]).then(function(results) {
+          if( results=== null ){
+             this.setState(function(){
+                 return{
+                     error: 'Oops something went wrong',
+                     loading: false
+                 }
+             })
+          }
+          else {
+              this.setState(function(){
+                  return {
+                      error: null,
+                      winner: results[0],
+                      loser: results[1],
+                      loading: false
+                  }
+              })
+          }
+        }.bind(this))
+    }
     render(){
-        console.log(this.props);
+        var winner = this.state.winner;
+        var loser = this.state.loser;
+        var error = this.state.error;
+        var loading = this.state.loading;
+
+        if(loading === true){
+           return <p>Loading...!!!</p>
+        }
+        if(error){
+           return (
+               <div>
+                   <p>{error}</p>
+                   <Link to="/battle">Reset</Link>
+               </div>
+           )
+        }
+        
         return(
             <div>
-                Results
+                {JSON.stringify(this.state, null, 2)}
             </div>
         )
     }
